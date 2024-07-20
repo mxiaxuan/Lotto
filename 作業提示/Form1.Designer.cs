d2b333397ff50fca73337402de7e49db58eda5fd
Mindy <st4u2636025252@gmail.com>
2024-07-20 15:36:32 +0800

    加入專案檔案。

---
+++ 作業提示/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace 作業提示
+{
+    partial class Fm作業提示
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOne = new System.Windows.Forms.Label();
+            this.lblTwo = new System.Windows.Forms.Label();
+            this.lblThree = new System.Windows.Forms.Label();
+            this.btn產生號碼 = new System.Windows.Forms.Button();
+            this.btn加入選號 = new System.Windows.Forms.Button();
+            this.btn刪除所選號碼 = new System.Windows.Forms.Button();
+            this.btn刪除所有號碼 = new System.Windows.Forms.Button();
+            this.lstb選號紀錄 = new System.Windows.Forms.ListBox();
+            this.lbl筆數 = new System.Windows.Forms.Label();
+            this.lblfour = new System.Windows.Forms.Label();
+            this.lblfive = new System.Windows.Forms.Label();
+            this.lblsix = new System.Windows.Forms.Label();
+            this.lblseven = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblOne
+            // 
+            this.lblOne.AutoSize = true;
+            this.lblOne.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOne.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblOne.Location = new System.Drawing.Point(12, 61);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(68, 76);
+            this.lblOne.TabIndex = 0;
+            this.lblOne.Text = "0";
+            // 
+            // lblTwo
+            // 
+            this.lblTwo.AutoSize = true;
+            this.lblTwo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTwo.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblTwo.Location = new System.Drawing.Point(148, 61);
+            this.lblTwo.Name = "lblTwo";
+            this.lblTwo.Size = new System.Drawing.Size(68, 76);
+            this.lblTwo.TabIndex = 1;
+            this.lblTwo.Text = "0";
+            // 
+            // lblThree
+            // 
+            this.lblThree.AutoSize = true;
+            this.lblThree.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblThree.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblThree.Location = new System.Drawing.Point(292, 61);
+            this.lblThree.Name = "lblThree";
+            this.lblThree.Size = new System.Drawing.Size(68, 76);
+            this.lblThree.TabIndex = 2;
+            this.lblThree.Text = "0";
+            this.lblThree.Click += new System.EventHandler(this.lblThree_Click);
+            // 
+            // btn產生號碼
+            // 
+            this.btn產生號碼.AutoSize = true;
+            this.btn產生號碼.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn產生號碼.Location = new System.Drawing.Point(411, 413);
+            this.btn產生號碼.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn產生號碼.Name = "btn產生號碼";
+            this.btn產生號碼.Size = new System.Drawing.Size(318, 50);
+            this.btn產生號碼.TabIndex = 3;
+            this.btn產生號碼.Text = "產生號碼";
+            this.btn產生號碼.UseVisualStyleBackColor = true;
+            this.btn產生號碼.Click += new System.EventHandler(this.btn產生號碼_Click);
+            // 
+            // btn加入選號
+            // 
+            this.btn加入選號.AutoSize = true;
+            this.btn加入選號.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn加入選號.Location = new System.Drawing.Point(18, 171);
+            this.btn加入選號.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn加入選號.Name = "btn加入選號";
+            this.btn加入選號.Size = new System.Drawing.Size(224, 50);
+            this.btn加入選號.TabIndex = 4;
+            this.btn加入選號.Text = "產生選號";
+            this.btn加入選號.UseVisualStyleBackColor = true;
+            this.btn加入選號.Click += new System.EventHandler(this.btn加入選號_Click);
+            // 
+            // btn刪除所選號碼
+            // 
+            this.btn刪除所選號碼.AutoSize = true;
+            this.btn刪除所選號碼.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn刪除所選號碼.Location = new System.Drawing.Point(18, 249);
+            this.btn刪除所選號碼.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn刪除所選號碼.Name = "btn刪除所選號碼";
+            this.btn刪除所選號碼.Size = new System.Drawing.Size(224, 50);
+            this.btn刪除所選號碼.TabIndex = 5;
+            this.btn刪除所選號碼.Text = "刪除所選號碼";
+            this.btn刪除所選號碼.UseVisualStyleBackColor = true;
+            this.btn刪除所選號碼.Click += new System.EventHandler(this.btn刪除所選號碼_Click);
+            // 
+            // btn刪除所有號碼
+            // 
+            this.btn刪除所有號碼.AutoSize = true;
+            this.btn刪除所有號碼.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn刪除所有號碼.Location = new System.Drawing.Point(18, 335);
+            this.btn刪除所有號碼.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn刪除所有號碼.Name = "btn刪除所有號碼";
+            this.btn刪除所有號碼.Size = new System.Drawing.Size(224, 50);
+            this.btn刪除所有號碼.TabIndex = 6;
+            this.btn刪除所有號碼.Text = "刪除所有號碼";
+            this.btn刪除所有號碼.UseVisualStyleBackColor = true;
+            this.btn刪除所有號碼.Click += new System.EventHandler(this.btn刪除所有號碼_Click);
+            // 
+            // lstb選號紀錄
+            // 
+            this.lstb選號紀錄.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lstb選號紀錄.FormattingEnabled = true;
+            this.lstb選號紀錄.ItemHeight = 35;
+            this.lstb選號紀錄.Location = new System.Drawing.Point(273, 171);
+            this.lstb選號紀錄.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstb選號紀錄.Name = "lstb選號紀錄";
+            this.lstb選號紀錄.Size = new System.Drawing.Size(576, 214);
+            this.lstb選號紀錄.TabIndex = 7;
+            // 
+            // lbl筆數
+            // 
+            this.lbl筆數.AutoSize = true;
+            this.lbl筆數.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl筆數.Location = new System.Drawing.Point(883, 171);
+            this.lbl筆數.Name = "lbl筆數";
+            this.lbl筆數.Size = new System.Drawing.Size(85, 35);
+            this.lbl筆數.TabIndex = 8;
+            this.lbl筆數.Text = "共0筆";
+            // 
+            // lblfour
+            // 
+            this.lblfour.AutoSize = true;
+            this.lblfour.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblfour.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblfour.Location = new System.Drawing.Point(438, 61);
+            this.lblfour.Name = "lblfour";
+            this.lblfour.Size = new System.Drawing.Size(68, 76);
+            this.lblfour.TabIndex = 9;
+            this.lblfour.Text = "0";
+            // 
+            // lblfive
+            // 
+            this.lblfive.AutoSize = true;
+            this.lblfive.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblfive.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblfive.Location = new System.Drawing.Point(582, 61);
+            this.lblfive.Name = "lblfive";
+            this.lblfive.Size = new System.Drawing.Size(68, 76);
+            this.lblfive.TabIndex = 10;
+            this.lblfive.Text = "0";
+            // 
+            // lblsix
+            // 
+            this.lblsix.AutoSize = true;
+            this.lblsix.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblsix.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblsix.Location = new System.Drawing.Point(712, 61);
+            this.lblsix.Name = "lblsix";
+            this.lblsix.Size = new System.Drawing.Size(68, 76);
+            this.lblsix.TabIndex = 11;
+            this.lblsix.Text = "0";
+            // 
+            // lblseven
+            // 
+            this.lblseven.AutoSize = true;
+            this.lblseven.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblseven.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblseven.Location = new System.Drawing.Point(866, 61);
+            this.lblseven.Name = "lblseven";
+            this.lblseven.Size = new System.Drawing.Size(68, 76);
+            this.lblseven.TabIndex = 12;
+            this.lblseven.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 35);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "第一區(1~38)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(802, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 35);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "第二區(1~8)";
+            // 
+            // Fm作業提示
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1011, 501);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblseven);
+            this.Controls.Add(this.lblsix);
+            this.Controls.Add(this.lblfive);
+            this.Controls.Add(this.lblfour);
+            this.Controls.Add(this.lbl筆數);
+            this.Controls.Add(this.lstb選號紀錄);
+            this.Controls.Add(this.btn刪除所有號碼);
+            this.Controls.Add(this.btn刪除所選號碼);
+            this.Controls.Add(this.btn加入選號);
+            this.Controls.Add(this.btn產生號碼);
+            this.Controls.Add(this.lblThree);
+            this.Controls.Add(this.lblTwo);
+            this.Controls.Add(this.lblOne);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Fm作業提示";
+            this.Text = "作業提示";
+            this.Load += new System.EventHandler(this.Fm作業提示_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOne;
+        private System.Windows.Forms.Label lblTwo;
+        private System.Windows.Forms.Label lblThree;
+        private System.Windows.Forms.Button btn產生號碼;
+        private System.Windows.Forms.Button btn加入選號;
+        private System.Windows.Forms.Button btn刪除所選號碼;
+        private System.Windows.Forms.Button btn刪除所有號碼;
+        private System.Windows.Forms.ListBox lstb選號紀錄;
+        private System.Windows.Forms.Label lbl筆數;
+        private System.Windows.Forms.Label lblfour;
+        private System.Windows.Forms.Label lblfive;
+        private System.Windows.Forms.Label lblsix;
+        private System.Windows.Forms.Label lblseven;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 作業提示/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 作業提示
+{
+
+    public partial class Fm作業提示 : Form
+    {
+        //類別變數
+        List<Label> listLebles = new List<Label>();
+        List<Label> listLebles1 = new List<Label>();
+        public Fm作業提示()
+        {
+            InitializeComponent();
+        }
+
+        private void Fm作業提示_Load(object sender, EventArgs e)
+        {
+            listLebles.Add(lblOne);
+            listLebles.Add(lblTwo);
+            listLebles.Add(lblThree);
+            listLebles.Add(lblfour);
+            listLebles.Add(lblfive);
+            listLebles.Add(lblsix);
+            listLebles1.Add(lblseven);
+        }
+
+        private void btn產生號碼_Click(object sender, EventArgs e)
+        {
+            Random myRnd = new Random();
+            //傳隨機數
+            //Console.WriteLine(random.Next());
+            ////傳出非負值的隨機整數，32位元
+
+            //Console.WriteLine(random.NextDouble());
+            ////輸出倍精度的隨機數
+
+            //Console.WriteLine(random.Next(100));
+            ////輸出0到99之間的任何一個整數
+
+            //Console.WriteLine(random.Next(1,39));
+            ////輸出1~38的任何一個整數
+            //Console.WriteLine(myRnd.Next(0, 10));
+
+            for (int i = 0; i < listLebles.Count; i++)
+            {
+                int randomNumber;
+                do
+                {
+                    randomNumber = myRnd.Next(1, 39);
+                } while (listLebles.Any(label => label.Text == randomNumber.ToString())); // 檢查是否重複
+
+                listLebles[i].Text = string.Format("{0}", randomNumber);
+            }
+
+            for (int i = 0; i < listLebles1.Count; i++)
+            {
+                listLebles1[i].Text = string.Format("{0}", myRnd.Next(1, 8));
+            }
+
+        }
+
+        private void lblThree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn加入選號_Click(object sender, EventArgs e)
+        {
+            string str選號 = "";
+            for (int i = 0; i < listLebles.Count; i++)
+            {
+                str選號 += listLebles[i].Text + "  ";
+            }
+            for (int i = 0; i < listLebles1.Count; i++)
+            {
+                str選號 += listLebles1[i].Text + "  ";
+            }
+            lstb選號紀錄.Items.Add(str選號);
+            顯示筆數();
+        }
+
+        private void btn刪除所選號碼_Click(object sender, EventArgs e)
+        {
+            if (lstb選號紀錄.SelectedIndex >= 0)
+            //如果所選號碼不是0
+            {
+                lstb選號紀錄.Items.RemoveAt(lstb選號紀錄.SelectedIndex);
+                //刪除所選號碼
+            }
+            else
+            {
+                MessageBox.Show("請選取要刪除號碼");
+            }
+            顯示筆數();
+        }
+
+        private void btn刪除所有號碼_Click(object sender, EventArgs e)
+        {
+            lstb選號紀錄.Items.Clear();
+            //將所有在lstb選號紀錄的Items都清除
+            顯示筆數();
+        }
+
+        void 顯示筆數()
+        {
+            lbl筆數.Text = string.Format("共{0}筆", lstb選號紀錄.Items.Count);
+        }
+    }
+}
